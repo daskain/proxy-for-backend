5388cc83a0e0f794e90fc569de3a087e4ec2b847
daskain <daskain@gmail.com>
2022-12-03 23:03:58 +0300

    fix

---
+++ src/main/java/com/example/pfb/ProxyForBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyForBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyForBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pfb/ProxyForBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyForBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
